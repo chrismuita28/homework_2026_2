bfd586b3c6e62a396097b1d04c8354bdb6c54186
zhaoyihua <zhaoyihua1@gmail.com>
2026-09-13 16:12:58 +0300

    Moved cache to the outer scope

--- source/factorial.js
+++ source/factorial.js
@@ -2,7 +2,7 @@
 
 /**
  * Функция, возвращающая факториал числа
- * В случае нечисловых входных данных возвращает NaN.
+ * В случае нечисловых входных данных возвращает NaN
  * В случае неккоректных числовых входных данных возвращает null
  * 
  * @param {number} n - целое число
@@ -13,30 +13,33 @@
  * 
  * @returns {number}
  */
-const factorial = (n) => {
-    if (typeof n !== 'number' || Number.isNaN(n)) {
-        return NaN;
-    }
-
-    if (!Number.isInteger(n) || n < 0) {
-        return null;
-    }
-
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    
+const factorial = (() => {
     const cache = [1, 1];
 
-    if (cache[n] !== undefined) {
-        return cache[n];
-    }
+    return (n) => {
+        if (typeof n !== 'number' || Number.isNaN(n)) {
+            return NaN;
+        }
 
-    let res = cache[cache.length - 1];
-    for (let i = cache.length; i <= n; i++) {
-        res *= i;
-        cache[i] = res;
-    }
+        if (!Number.isInteger(n) || n < 0) {
+            return null;
+        }
 
-    return res;
-};
+        if (n === 0 || n === 1) {
+            return 1;
+        }
+        
+
+        if (cache[n] !== undefined) {
+            return cache[n];
+        }
+
+        let res = cache[cache.length - 1];
+        for (let i = cache.length; i <= n; i++) {
+            res *= i;
+            cache[i] = res;
+        }
+
+        return res;
+    };
+})();
