dea2028cde2b9f62104e2dec7257fa8bff687f8e
zhaoyihua <zhaoyihua1@gmail.com>
2026-09-13 16:42:21 +0300

    Moved cache out of the function

--- source/factorial.js
+++ source/factorial.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /**
+ * Кеш для хранения уже вычисленных значений факториала
+ * Инициализируется базовыми значениями: 0! = 1 и 1! = 1
+ * @type {number[]}
+ */
+const cache = [1, 1];
+
+/**
  * Функция, возвращающая факториал числа
  * В случае нечисловых входных данных возвращает NaN
  * В случае неккоректных числовых входных данных возвращает null
@@ -13,33 +20,24 @@
  * 
  * @returns {number}
  */
-const factorial = (() => {
-    const cache = [1, 1];
+const factorial = (n) => {
+    if (typeof n !== 'number' || Number.isNaN(n)) {
+        return NaN;
+    }
 
-    return (n) => {
-        if (typeof n !== 'number' || Number.isNaN(n)) {
-            return NaN;
-        }
+    if (!Number.isInteger(n) || n < 0) {
+        return null;
+    }
 
-        if (!Number.isInteger(n) || n < 0) {
-            return null;
-        }
+    if (cache[n] !== undefined) {
+        return cache[n];
+    }
 
-        if (n === 0 || n === 1) {
-            return 1;
-        }
-        
+    let res = cache[cache.length - 1];
+    for (let i = cache.length; i <= n; i++) {
+        res *= i;
+        cache[i] = res;
+    }
 
-        if (cache[n] !== undefined) {
-            return cache[n];
-        }
-
-        let res = cache[cache.length - 1];
-        for (let i = cache.length; i <= n; i++) {
-            res *= i;
-            cache[i] = res;
-        }
-
-        return res;
-    };
-})();
+    return res;
+};
